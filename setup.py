dd52281e8562a003ec5e8cbb44e2a9537d203a7b
Unix <mb_wali@hotmail.com>
2021-02-18 08:53:31 +0100

    Merge remote-tracking branch 'origin/master' into permissions-376

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2019-2021 CERN.
 # Copyright (C) 2019-2021 Northwestern University.
 # Copyright (C)      2021 TU Wien.
-# Copyright (C)      2021 Graz University of Technology.
 #
 # Invenio-RDM-Records is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -20,6 +19,7 @@ history = open('CHANGES.rst').read()
 tests_require = [
     'pytest-invenio>=1.4.1,<2.0.0',
     'invenio-app>=1.3.0,<2.0.0',
+    'pytest-mock>=1.6.0',
 ]
 
 # Should follow inveniosoftware/invenio versions
